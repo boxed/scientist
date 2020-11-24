f16022d37e961ae46429f523439617940836cb7e
Anders Hovmöller <anders.hovmoller@trioptima.com>
2020-11-24 13:37:30 +0100

    .

--- mutmut3.py
+++ mutmut3.py
@@ -25,65 +25,92 @@ def create_mutants():
     return next_id
 
 
-def create_mutants_for_file(filename, next_id):
-    output_path = Path('mutants') / filename
-    makedirs(output_path.parent, exist_ok=True)
+def write_trampoline(out, c, orig_name, mutant_names):
+    print(file=out)
+    print(f'{orig_name}_mutants = {{' + ', '.join(f'"{i}": {m}' for i, m in enumerate(mutant_names)) + '}', file=out)
 
-    with open(output_path, 'w') as out:
-        # start = datetime.now()
-        c = Context(filename=filename)
-        orig_name = c.ast.children[0].name.value
-        c.ast.children[0].name.value += '_orig'
-        print(c.get_code(), file=out)
-        c.ast.children[0].name.value = orig_name
-
-        mutation_ids = list_mutations(c)
-        mutant_names = []
-
-        for mutation_id in mutation_ids:
-            print(file=out)
-            c.mutation_id = mutation_id
-            new_code, number = mutate(c)
-            c.ast.children[0].name.value += f'_mutant_{next_id}'
-            mutant_names.append(c.ast.children[0].name.value)
-            assert number == 1, number
-            print(c.get_code(), file=out)
-            c.ast.children[0].name.value = orig_name
-
-            next_id += 1
-
-        print(file=out)
-        print(f'{orig_name}_mutants = {{' + ', '.join(f'"{i}": {m}' for i, m in enumerate(mutant_names)) + '}', file=out)
-
-        # trampoline
-        print("""
+    # trampoline
+    print("""
 
 def trampoline(orig, mutants, *args, **kwargs):
     import os
     mutant_under_test = os.environ.get('MUTANT_UNDER_TEST', None)
     return mutants.get(mutant_under_test, orig)(*args, **kwargs)""", file=out)
 
-        f = c.ast.children[0]
-        signature = f.children[2].get_code()
+    f = c.ast.children[0]
+    signature = f.children[2].get_code()
 
-        # this is actually wrong, because the signature can be "(foo, *, bar)" which is invalid at the call site, need to fix this at some point
-        call = signature[1:-1]  # the slice is to remove the parens
+    # this is actually wrong, because the signature can be "(foo, *, bar)" which is invalid at the call site, need to fix this at some point
+    call = signature[1:-1]  # the slice is to remove the parens
 
-        # Replace body with trampoline
-        trampoline_ast = parse(f"""
+    # Replace body with trampoline
+    trampoline_ast = parse(f"""
 def {orig_name}{signature}:
     return trampoline({orig_name}_orig, {orig_name}_mutants, {call})""")
 
-        f.children[-1] = trampoline_ast.children[0].children[-1]
+    f.children[-1] = trampoline_ast.children[0].children[-1]
 
-        print(file=out)
-        print(file=out)
-        print(f.get_code(), file=out)
+    print(file=out)
+    print(file=out)
+    print(f.get_code(), file=out)
 
-        # print()
-        #
-        # t = datetime.now() - start
-        # print(len(mutation_ids), len(mutation_ids) / t.total_seconds(), 'mutations per s')
+
+def write_mutant(out, c, mutation_id, next_id, mutant_names, orig_name):
+    print(file=out)
+    c.mutation_id = mutation_id
+    new_code, number = mutate(c)
+    c.ast.children[0].name.value += f'_mutant_{next_id}'
+    mutant_names.append(c.ast.children[0].name.value)
+    assert number == 1, number
+    print(c.get_code(), file=out)
+    c.ast.children[0].name.value = orig_name
+
+
+def write_original(out, foo):
+    last_subject, source = foo
+    orig_name = last_subject.name.value
+    print(source, file=out)
+    print(f'{orig_name}_orig = {orig_name}', file=out)  # the trampoline will then overwrite the original
+
+
+def create_mutants_for_file(filename, next_id):
+    output_path = Path('mutants') / filename
+    makedirs(output_path.parent, exist_ok=True)
+
+    with open(output_path, 'w') as out:
+        c = Context(filename=filename)
+
+        mutation_ids = list_mutations(c)
+
+        mutant_names = []
+        last_subject = None
+
+        for mutation_id in mutation_ids:
+            if not mutation_id.subject:
+                continue
+
+            if mutation_id.subject[0].type != 'funcdef':
+                continue
+
+            if last_subject != mutation_id.subject:
+                if mutant_names:
+                    write_trampoline(out, c, orig_name, mutant_names)
+
+                orig_name = mutation_id.subject[0].name.value
+                if last_subject:
+                    write_original(out, last_subject)
+                mutant_names = []
+                last_subject = mutation_id.subject
+
+            write_mutant(out, c, mutation_id, next_id, mutant_names, orig_name)
+
+            next_id += 1
+
+        if last_subject:
+            write_original(out, last_subject)
+
+        if mutant_names:
+            write_trampoline(out, c, orig_name, mutant_names)
 
     return next_id
 
@@ -125,6 +152,10 @@ def mutmut_3():
         if not pid:
             # In the child
             os.environ['MUTANT_UNDER_TEST'] = str(key)
+
+            # TODO: this is needed for non-memory DBs
+            # hammett.Config.workerinput = dict(workerinput=f'_{key}')
+
             result = hammett.main_run_tests(**hammett_kwargs)
             if result != 0:
                 # TODO: write failure information to stdout?
